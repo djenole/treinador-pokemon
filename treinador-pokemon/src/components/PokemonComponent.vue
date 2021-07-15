3e423f2a29acecc5890a7b5f4945b9e6d4081f63
djenole <55561761+djenole@users.noreply.github.com>
2021-07-15 14:01:30 -0300

    Criado o pokemon componente root

---
+++ treinador-pokemon/src/components/PokemonComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="pokemon-component">
+        <h1>Pokemon's</h1>
+    </div>
+</template>
+
+<script>
+
+
+export default({
+    name: 'PokemonComponent',
+    
+    setup() {       
+    },
+})
+</script>
+
+<style scoped>
+
+</style>
